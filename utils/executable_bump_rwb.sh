0d9b3443ba207cf03db95fbf452d4484983656ab
Russell Blickhan <rwblickhan@gmail.com>
2025-11-06 09:40:25 -0800

    Add a few missing items

---
+++ utils/executable_bump_rwb.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Bump rwblickhan.org
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.author rwblickhan
+# @raycast.authorURL https://raycast.com/rwblickhan
+
+fish -c bump_rwb
