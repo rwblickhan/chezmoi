e47744b1aecfa0c3422751e57c2897a673264d48
Russell Blickhan <rwblickhan@gmail.com>
2025-12-11 10:07:57 -0800

    Add left/right arrow Hammerspoon config

--- dot_hammerspoon/init.lua
+++ dot_hammerspoon/init.lua
@@ -10,6 +10,16 @@ local function sendUpArrow()
     hs.eventtap.keyStroke({}, "up")
 end
 
+local function sendRightArrow()
+    hs.eventtap.keyStroke({}, "right")
+end
+
+local function sendLeftArrow()
+    hs.eventtap.keyStroke({}, "left")
+end
+
 hs.hotkey.bind({ "cmd", "ctrl", "alt", "shift" }, "j", sendDownArrow)
 hs.hotkey.bind({ "cmd", "ctrl", "alt", "shift" }, "k", sendUpArrow)
+hs.hotkey.bind({ "cmd", "ctrl", "alt", "shift" }, "h", sendLeftArrow)
+hs.hotkey.bind({ "cmd", "ctrl", "alt", "shift" }, "l", sendRightArrow)
 print("Arrow key navigation config created!")
